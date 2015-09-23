e1f013d8fcb17dbdc48fd00b42949ae33230fc62
None <b1504ha@aiit.ac.jp>
2015-09-23 06:28:38 +0000

    Iot message

--- hello.rb
+++ hello.rb
@@ -1,1 +1,18 @@
-hello
+require 'sinatra'
+
+get '/' do
+ "Hello World!"
+end
+
+get '/AIIT' do
+ "Hello AIIT!"
+end
+
+get '/iot/morning" do
+ "Good Morning!"
+end
+
+get '/iot/evening' do
+ "Good evening!"
+end
+
