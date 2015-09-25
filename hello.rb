6821d20eba6d172624a786962031c9d2f6a9f9aa
sazanaminoya <sazanaminoya@users.noreply.github.com>
2015-09-25 16:49:38 +0900

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
-require 'sinatra'
-require 'rakuten_web_service'
+# -*- coding: utf-8 -*- 
+require 'sinatra' 
+require 'rakuten_web_service' 
 
-#参考: https://github.com/k2works/sinatra_rakuten_api
+#参考: https://github.com/k2works/sinatra_rakuten_api 
 
-get '/' do
-  RakutenWebService.configuration do |c|
-    c.application_id = 1038224467908657119
-    c.affiliate_id = "145c3657.d691dece.145c3658.016c6745"
-  end
 
-  # Use genre id to fetch genre object
-  @rankings = RakutenWebService::Ichiba::Item.ranking(:age => 20, :sex => 0)
-  @rankings = RakutenWebService::Ichiba::Genre[100337].ranking
-  erb :item_ranking
-end
+get '/' do 
+  RakutenWebService.configuration do |c| 
+    c.application_id = ENV["APPID"] 
+    c.affiliate_id = ENV["AFID"] 
+end 
+ 
+# Use genre id to fetch genre object 
+   @rankings = RakutenWebService::Ichiba::Item.ranking(:genreId => 100337) 
+   erb :item_ranking 
+end 
